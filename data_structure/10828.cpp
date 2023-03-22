b09e52ac36c6e4056cf6029dfecdcba53a901e51
yyeonzu <tooma22@naver.com>
2023-03-22 13:58:49 +0900

    0322

--- data_structure/10828.cpp
+++ data_structure/10828.cpp
@@ -1,16 +1,10 @@
-// stack STL을 사용하면 된다
-
 #include <iostream>
 #include <string>
 #include <stack>
 
 using namespace std;
 
-// push, pop, size, empty, top을 구현
-
-// 명령 개수로 while문돌리기
-// cin의 값에 따라 stack의 메서드 사용하기
-
+// stack STL 사용
 int main()
 {
     string s;
@@ -32,28 +26,23 @@ int main()
         else if (s == "pop")
         {
             if (st.empty())
-                cout << 0;
-            else
-                st.pop();
-        }
-        else if (s == "size")
-        {
-            cout << st.size();
-        }
-        else if (s == "empty")
-        {
-            cout << st.empty();
-        }
-        else
-        {
-            if (st.empty())
-            {
                 cout << -1;
-            }
             else
             {
                 cout << st.top();
+                st.pop();
             }
+        }
+        else if (s == "size")
+            cout << st.size();
+        else if (s == "empty")
+            cout << st.empty();
+        else
+        {
+            if (st.empty())
+                cout << -1;
+            else
+                cout << st.top();
         }
         cout << "\n";
     }

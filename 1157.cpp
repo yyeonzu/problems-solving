9a7a568a404b85e7b1342767dc7c00c6d9e36e7e
yyeonzu <tooma22@naver.com>
2023-05-22 19:18:57 +0900

    backup

---
+++ 1157.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string alpha = "abcdefghijklmnopqrstuvwxyz";
+	string str;
+	
+	cin >> str;
+
+	for (int i = 0; str[i]; i++)
+	{
+	}
+}
